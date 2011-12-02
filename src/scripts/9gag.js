5fd1c276aff984dc4da6facb710ccf01d9d5e1a8
Johnny G. Halife <johnny.halife@me.com>
2011-12-01 23:18:04 -0300

    using `hubot 9gag` instead of just hear

--- src/scripts/9gag.js
+++ src/scripts/9gag.js
@@ -1,11 +1,11 @@
 // Random meme from 9gag (9gag.com)
 //
-// 9gag  -	returns a random meme
+// hubot 9gag  -	returns a random meme
 
 var memeDomain = "http://9gag.com";
 
 module.exports = function(robot) {
-	robot.hear(/9gag/i, function(msg){
+	robot.respond(/9gag/i, function(msg){
 		msg.http(memeDomain + "/random").get()(function(e, r, b) {
 			msg.send(memeDomain + r.headers['location']);
 		});
