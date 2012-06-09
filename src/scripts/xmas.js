6334eeb127e05076e926fa832c6adf93c7eb6d52
Josh Nichols <josh@technicalpickles.com>
2012-06-08 20:47:20 -0400

    Update "x" help comments

--- src/scripts/xmas.js
+++ src/scripts/xmas.js
@@ -1,7 +1,19 @@
-// xmas detector script
 //
-// hubot is it xmas ?  - returns whether is it christmas or not
-// hubot is it christmas ?  - returns whether is it christmas or not
+// Description:
+//   xmas detector script
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot is it xmas ?  - returns whether is it christmas or not
+//   hubot is it christmas ?  - returns whether is it christmas or not
+//
+// Author:
+//   Johnny G. Halife
 //
 
 module.exports = function(robot) {
